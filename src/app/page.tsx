aa126000fb442b8a94f4696922d65828067c581f
Nishant Kango <contact@nishantkango.com>
2025-05-23 17:34:00 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,10 @@
-// import { Suspense } from "react";
-// import YourComponent from "./InterviewPageCient";
+import { Suspense } from "react";
+import YourComponent from "./InterviewPageCient";
 
-// export default function Page() {
-//   return (
-//     <Suspense fallback={<div>Loading...</div>}>
-//       <YourComponent />
-//     </Suspense>
-//   );
-// }
-
-
-"use client";
- 
-import React from "react";
-import App from "./App";
-import { TranscriptProvider } from "../app/contexts/TranscriptContext";
-import { EventProvider } from "../app/contexts/EventContext";
- 
-export default function InterviewPage() {
-  const batchId: string = "213324dwfdwsdf";
-  const jobId: string = "wfwefvwe";
- 
+export default function Page() {
   return (
-    <TranscriptProvider>
-      <EventProvider>
-        <App batch_id={batchId} job_id={jobId} />
-      </EventProvider>
-    </TranscriptProvider>
+    <Suspense fallback={<div>Loading...</div>}>
+      <YourComponent />
+    </Suspense>
   );
 }
